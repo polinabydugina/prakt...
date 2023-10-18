03823d36590216fdd7c906e4941aca317ba98727
212224 <xch.victor@mail.ru>
2023-10-18 16:47:36 +0300

    Добавлена фабрика и классы теперь отдельно

--- 55/Program.cs
+++ 55/Program.cs
@@ -1,14 +1,16 @@
 ﻿using Newtonsoft.Json;
-Product apple = new Product("30g", "20", "5", "apple", "no");
-Product potatoes = new Product("1kg", "55", "2", "potatoes", "no");
-Product lemon = new Product("150g", "50", "3", "lemon", "no");
-Product glass = new Product("300g", "50", "200ml", "glass", "yes");
-Product table = new Product("1.5kg", "2000", "12", "table", "no");
-Product vaza = new Product("2kg", "1500", "2l", "vaza", "yes");
-Product milk = new Product("500ml", "100", "500ml", "milk", "no");
-Product flower = new Product("300g", "350", "12", "flower", "yes");
-Product limonade = new Product("1l", "100", "1l", "limonade", "no");
-Product cake = new Product("600g", "400", "200", "cake", "no");
+using class1;
+
+Product apple = Factory.Create("30g", "20", "5", "apple", "no");
+Product potatoes = Factory.Create("1kg", "55", "2", "potatoes", "no");
+Product lemon = Factory.Create("150g", "50", "3", "lemon", "no");
+Product glass = Factory.Create("300g", "50", "200ml", "glass", "yes");
+Product table = Factory.Create("1.5kg", "2000", "12", "table", "no");
+Product vaza = Factory.Create("2kg", "1500", "2l", "vaza", "yes");
+Product milk = Factory.Create("500ml", "100", "500ml", "milk", "no");
+Product flower = Factory.Create("300g", "350", "12", "flower", "yes");
+Product limonade = Factory.Create("1l", "100", "1l", "limonade", "no");
+Product cake = Factory.Create("600g", "400", "200", "cake", "no");
 
 List<Product> products = new List<Product>();
 products.Add(apple);
@@ -26,22 +28,4 @@ foreach (Product c in products)
 {
     string json = JsonConvert.SerializeObject(c);
     Console.WriteLine(json);
-}
-class Product
-{
-    public string weight;
-    public string cost;
-    public string volume;
-    public string name;
-    public string fragility;
-
-    public Product(string weight, string cost, string volume, string name, string fragility)
-    {
-
-        this.weight = weight;
-        this.cost = cost;
-        this.volume = volume;
-        this.name = name;
-        this.fragility = fragility;
-    }
 }
---
+++ 55/class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+    public class Product
+    {
+        public string weight;
+        public string cost;
+        public string volume;
+        public string name;
+        public string fragility;
+
+        public Product(string weight, string cost, string volume, string name, string fragility)
+        {
+
+            this.weight = weight;
+            this.cost = cost;
+            this.volume = volume;
+            this.name = name;
+            this.fragility = fragility;
+        }
+    }
+
+    public static class Factory
+    {
+        public static Product Create(string weight, string cost, string volume, string name, string fragility)
+        {
+            return new Product(weight, cost, volume, name, fragility);
+        }
+    }
+}
